75b8ab6fbc51f2c09f1a488a26dfe74552fd9284
WonHa Baeg <48233467+watual@users.noreply.github.com>
2024-04-24 17:31:37 +0900

    과제진행1.2
    #개인과제 요구사항 1.2 수행
    1.2 Scanner를 사용하여 사칙연산 기호 전달받기

--- spartaAssignment/src/App.java
+++ spartaAssignment/src/App.java
@@ -6,6 +6,11 @@ public class App {
         System.out.print("첫 번째 숫자를 입력하세요: ");
         // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
         int num1 = Integer.parseInt(sc.nextLine());
+
+        System.out.print("사칙연산 기호를 입력하세요: ");
+        // 사칙연산 기호를 적합한 타입으로 선언한 변수에 저장합니다.
+        String oper = sc.nextLine();
+
         System.out.print("두 번째 숫자를 입력하세요: ");
         // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
         int num2 = Integer.parseInt(sc.nextLine());
