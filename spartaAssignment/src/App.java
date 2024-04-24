a1a2567ee54d9bb6908f22d7d7d3913b36a38198
WonHa Baeg <48233467+watual@users.noreply.github.com>
2024-04-24 17:28:56 +0900

    Java 프로젝트 생성 및 과제진행1.1
    #Java 프로젝트 생성 _ JDK22
    #계산기 만들기 개인과제 시작
    
    #개인과제 요구사항 1.1 수행
    1.1 Scanner를 사용해 정수 2개 수령

---
+++ spartaAssignment/src/App.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("첫 번째 숫자를 입력하세요: ");
+        // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
+        int num1 = Integer.parseInt(sc.nextLine());
+        System.out.print("두 번째 숫자를 입력하세요: ");
+        // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
+        int num2 = Integer.parseInt(sc.nextLine());
+    }
+}
