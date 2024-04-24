9465468592baf825d4fe7dac2c2c9613f22609fa
WonHa Baeg <48233467+watual@users.noreply.github.com>
2024-04-24 17:41:10 +0900

    과제진행1.3
    #개인과제 요구사항 1.3 수행
    1.3 입력받은 데이터 연살 실행 후 결과값 출력(제어문 사용)
     - 연산 오류시 내용 정제하여 출력(try, catch)

--- spartaAssignment/src/App.java
+++ spartaAssignment/src/App.java
@@ -1,3 +1,4 @@
+import java.io.IOException;
 import java.util.Scanner;
 
 public class App {
@@ -14,5 +15,20 @@ public class App {
         System.out.print("두 번째 숫자를 입력하세요: ");
         // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
         int num2 = Integer.parseInt(sc.nextLine());
+
+        int result = 0;
+        /* 제어문을 활용하여 위 요구사항을 만족할 수 있게 구현합니다.*/
+        try {
+            switch (oper) {
+                case "+" -> result = num1 + num2;
+                case "-" -> result = num1 - num2;
+                case "*" -> result = num1 * num2;
+                case "/" -> result = num1 / num2;
+            }
+        }catch (ArithmeticException e){
+            System.out.println(e.getClass());
+            System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+        }
+        System.out.println(STR."결과: \{result}");
     }
 }
