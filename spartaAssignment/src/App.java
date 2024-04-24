550699e4896cedacaa67b99296e994fa837f7bf2
WonHa Baeg <48233467+watual@users.noreply.github.com>
2024-04-24 17:44:48 +0900

    과제진행1.4
    #코드 레벨 정리
    #개인과제 요구사항 1.4 수행
    1.4 반복문을 사용하여 반복의 종료를 알려주는 “exit” 문자열을 입력하기 전까지 무한으로 계산을 진행할 수 있도록 소스 코드를 수정(while)

--- spartaAssignment/src/App.java
+++ spartaAssignment/src/App.java
@@ -4,31 +4,41 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("첫 번째 숫자를 입력하세요: ");
-        // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
-        int num1 = Integer.parseInt(sc.nextLine());
+        /* 반복문 사용 해서 연산을 반복 */
+        while (true) {
+            System.out.print("첫 번째 숫자를 입력하세요: ");
+            // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
+            int num1 = Integer.parseInt(sc.nextLine());
 
-        System.out.print("사칙연산 기호를 입력하세요: ");
-        // 사칙연산 기호를 적합한 타입으로 선언한 변수에 저장합니다.
-        String oper = sc.nextLine();
+            System.out.print("사칙연산 기호를 입력하세요: ");
+            // 사칙연산 기호를 적합한 타입으로 선언한 변수에 저장합니다.
+            String oper = sc.nextLine();
 
-        System.out.print("두 번째 숫자를 입력하세요: ");
-        // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
-        int num2 = Integer.parseInt(sc.nextLine());
+            System.out.print("두 번째 숫자를 입력하세요: ");
+            // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
+            int num2 = Integer.parseInt(sc.nextLine());
 
-        int result = 0;
-        /* 제어문을 활용하여 위 요구사항을 만족할 수 있게 구현합니다.*/
-        try {
-            switch (oper) {
-                case "+" -> result = num1 + num2;
-                case "-" -> result = num1 - num2;
-                case "*" -> result = num1 * num2;
-                case "/" -> result = num1 / num2;
+            int result = 0;
+            /* 제어문을 활용하여 위 요구사항을 만족할 수 있게 구현합니다.*/
+            try {
+                switch (oper) {
+                    case "+" -> result = num1 + num2;
+                    case "-" -> result = num1 - num2;
+                    case "*" -> result = num1 * num2;
+                    case "/" -> result = num1 / num2;
+                }
+            } catch (ArithmeticException e) {
+                System.out.println(e.getClass());
+                System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
             }
-        }catch (ArithmeticException e){
-            System.out.println(e.getClass());
-            System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+            System.out.println(STR."결과: \{result}");
+
+            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+            /* exit을 입력 받으면 반복 종료 */
+            if ( sc.nextLine().equals("exit") ){
+                break;
+            }
+
         }
-        System.out.println(STR."결과: \{result}");
     }
 }
